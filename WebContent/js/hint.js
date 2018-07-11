1f0d14a0419c79e954507abae79c0bf77c46d616
Kalkihe <t.malina@bitmessage.ch>
2018-07-11 08:20:03 +0200

    added addHInt() for event Listener, removed automatic show of hint

--- WebContent/js/hint.js
+++ WebContent/js/hint.js
@@ -1,7 +1,9 @@
-window.addEventListener('load', hint);
-document.getElementById('hintsign').addEventListener('click', hint);
-
 function hint()
 {
   alert("Ziel des Spieles ist es, drei gleiche Symbole in einer Reihe, Spalte oder Diagonal zu setzen. Die Spieler sind abwechselnd am Zug.");
+}
+
+function addHint()
+{
+  document.getElementById("hintsign").addEventListener('click', hint);
 }
