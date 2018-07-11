a687c92326d159f256ae5ecec3a4e0f0d35abde6
Kalkihe <t.malina@bitmessage.ch>
2018-07-11 08:19:34 +0200

    changed name of canvas

--- WebContent/js/field.js
+++ WebContent/js/field.js
@@ -1,20 +1,20 @@
 function field(){
-	var c = document.getElementById("singlePlayerCanvas");
+	var c = document.getElementById("gameCanvas");
 	var ctx = c.getContext("2d");
 	ctx.moveTo(300, 0);
 	ctx.lineTo(300, 600);
 	ctx.stroke();
-	var d = document.getElementById("singlePlayerCanvas");
+	var d = document.getElementById("gameCanvas");
 	var dtx = c.getContext("2d");
 	dtx.moveTo(00, 200);
 	dtx.lineTo(900, 200);
 	dtx.stroke();
-	var e = document.getElementById("singlePlayerCanvas");
+	var e = document.getElementById("gameCanvas");
 	var etx = c.getContext("2d");
 	etx.moveTo(600, 0);
 	etx.lineTo(600, 600);
 	etx.stroke();
-	var f = document.getElementById("singlePlayerCanvas");
+	var f = document.getElementById("gameCanvas");
 	var ftx = c.getContext("2d");
 	ftx.moveTo(00, 400);
 	ftx.lineTo(900, 400);
