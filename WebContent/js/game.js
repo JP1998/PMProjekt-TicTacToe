b50f77eee218b6d13f14323172bc781c15ccafbd
Kalkihe <t.malina@bitmessage.ch>
2018-07-11 08:44:29 +0200

    implemented function for calculating the field number

--- WebContent/js/game.js
+++ WebContent/js/game.js
@@ -1,15 +1,23 @@
-// TODO: Event-Listener für Canvas implementieren
-
+// Initialisiert das Spiel, indem es das dem Canvas den Event-Listener hinzufügt.
 function initGame()
 {
   document.getElementById('gameCanvas').addEventListener('click', handleCickOnCanvas);
 }
 
+// Funktion für das Klick-Ereignis auf dem Spielfeld
 function handleCickOnCanvas(event)
 {
   x = event.offsetX;
   y = event.offsetY;
-  alert("X: " + x + " Y: " + y);
+}
+
+// Berechnet anhand der übergebenen Koordinaten die Nummer des Feldes und gibt diese zurück.
+function getField(x, y)
+{
+  field = Math.floor(x / 300);
+  field = field + 3 * Math.floor(y / 200);
+  field = field + 1;
+  return field;
 }
 // TODO: XMLHttpRequest Objekt setzen
 // TODO: Aktualisierung des Feldes bearbeiten
