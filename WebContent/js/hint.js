fda2d93c28b985b2e69ed83bcf605904da1ca083
Jonny <littlebigjonny06@gmail.com>
2018-07-16 11:16:05 +0200

    Add method for dismissing the hint

--- WebContent/js/hint.js
+++ WebContent/js/hint.js
@@ -1,7 +1,12 @@
-function hint() {
-  alert("Ziel des Spieles ist es, als erster drei gleiche Symbole in einer Reihe, Spalte oder Diagonal zu setzen. Die Spieler sind abwechselnd am Zug.");
-}
 
 function addHint() {
-  document.getElementById("hintsign").addEventListener('click', hint);
+  document.getElementById("hintsign").addEventListener('click', showHint);
+}
+
+function showHint() {
+	alert("Ziel des Spieles ist es, als erster drei gleiche Symbole in einer Reihe, Spalte oder Diagonal zu setzen. Die Spieler sind abwechselnd am Zug.");
+}
+
+function dismissHint() {
+	
 }
