c8a4ded8d964f4c9c3e918a0789c14347ace095b
Jonny <littlebigjonny06@gmail.com>
2018-07-16 10:35:12 +0200

    Add FAQ to server

---
+++ src/de/tictactoecomp/game/server/FAQServlet.java
@@ -0,0 +1,29 @@
+package de.tictactoecomp.game.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.tictactoecomp.game.server.abstr.LoggingServlet;
+
+@WebServlet("/faq")
+public class FAQServlet extends LoggingServlet {
+
+    public FAQServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/faq.html")
+            .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log("User with sessionId '{0}' has requested /faq via POST request.", request.getSession().getId());
+        response.setStatus(405);
+    }
+}
